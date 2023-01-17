a7d808fe9b584846a785caddcfd6d1d5c6c718ec
Prashant <prashantamoli2621@gmail.com>
2023-01-17 21:03:08 +0530

    SellerAuth++

--- src/contracts/SellerAuth.sol
+++ src/contracts/SellerAuth.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Auth {
+contract SellerAuth {
     address private owner;
     // Owners password
     string private secret;
