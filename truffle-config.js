2e915cd13bac690e566efe183384862fb4bcb4e6
Via5K <1neerajnautiyal@gmail.com>
2023-05-21 18:53:29 +0530

    Sepolia Migration --Failed

--- truffle-config.js
+++ truffle-config.js
@@ -57,12 +57,25 @@ module.exports = {
             networkCheckTimeout: 10000,
             timeoutBlocks: 2000,
             skipDryRun: true,
-            provider: function() {
+            provider: function () {
                 return new HDWalletProvider(MNEMONIC, API_URL)
             },
             network_id: 5,
             // gas: 60000000 //GAS FEE
             // gas: 4000000 //4M is the max
+        },
+        sepolia: {
+            networkCheckTimeout: 10000,
+            timeoutBlocks: 2000,
+            skipDryRun: true,
+            provider: function () {
+                return new HDWalletProvider(MNEMONIC, API_URL)
+            },
+            network_id: 11155111,
+
+            // gas: 60000000 //GAS FEE
+            // gas: 4000000 //4M is the max
+
         }
 
         // Another network with more advanced options...
